8d86438535ab300d8c47f43dca04985fd651968f
Atanas Yonkov <a.yonkov@student.fontys.nl>
2021-03-05 12:42:54 +0200

    rigidbody implementation for jumping and moving

---
+++ Assets/newMoveJump.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newMoveJump : MonoBehaviour
+{
+    public float moveSpeed;
+    private float xInput, yInput;
+
+    Rigidbody2D rb;
+
+    SpriteRenderer sp;
+
+    public float jumpForce;
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        transform.Translate(xInput * moveSpeed, yInput * moveSpeed, 0);
+
+        PlatformerMove();
+    }
+
+    void Jump()
+    {
+        rb.velocity = Vector2.up * jumpForce;
+    }
+
+    void PlatformerMove()
+    {
+        rb.velocity = new Vector2(moveSpeed * xInput, rb.velocity.y);
+    }
+
+}
