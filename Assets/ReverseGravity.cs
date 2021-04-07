3caa1b203dc2854dcce8691feda0fd780980071b
Hristo Koltchakov <h.koltchakov@student.fontys.nl>
2021-04-07 10:46:44 +0300

    Added reverse gravity script

---
+++ Assets/ReverseGravity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseGravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().gravityScale = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
