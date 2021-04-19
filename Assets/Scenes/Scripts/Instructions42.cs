c61b22979bb3630df7163bc7243bb0ecc952a104
Hristo Koltchakov <h.koltchakov@student.fontys.nl>
2021-04-19 16:06:36 +0300

    Fixed Camera added Tooltips and 3rd level add-ons

---
+++ Assets/Scenes/Scripts/Instructions42.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Instructions42 : MonoBehaviour
+{
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Subject42")
+        {
+            uiObject.SetActive(true);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        uiObject.SetActive(false);
+    }
+}   
---
+++ Assets/Scenes/Scripts/XInstructions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XInstructions : MonoBehaviour
+{
+    public GameObject uiObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "SubjectX")
+        {
+            uiObject.SetActive(true);
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        uiObject.SetActive(false);
+    }
+}
